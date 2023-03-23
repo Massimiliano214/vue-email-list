7fd49670480f805a83bdba65aa3da3bd72764adb
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-03-23 16:33:04 +0100

    load every email at the same time

--- js/main.js
+++ js/main.js
@@ -3,22 +3,14 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      emailGenerated: [
-
-      ]
+      emailGenerated: []
     }
-  },
-  methods: {
-    
   },
   mounted() {
     for (i = 0; i < 10; i++) {
-        console.log("ciao" + this.emailGenerated);
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then( (response) => {
             console.log(response);
-            //this.emailGenerated.primaEmail = response.data.response;
-
             this.emailGenerated.push(response.data.response);
             console.log(this.emailGenerated);
             console.log(this.emailGenerated.length);
