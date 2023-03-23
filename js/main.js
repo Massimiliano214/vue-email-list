6f9ec08b0e3e834f3e6530dba84d5384c31df48e
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-03-23 16:23:23 +0100

    working email generation

--- js/main.js
+++ js/main.js
@@ -9,18 +9,20 @@ createApp({
     }
   },
   methods: {
-    repeatEmail() {
-        
-    }
+    
   },
   mounted() {
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then( (response) => {
-        console.log(response);
-        //this.emailGenerated.primaEmail = response.data.response;
+    for (i = 0; i < 10; i++) {
+        console.log("ciao" + this.emailGenerated);
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( (response) => {
+            console.log(response);
+            //this.emailGenerated.primaEmail = response.data.response;
 
-        this.emailGenerated.push(response.data.response);
-        console.log(this.emailGenerated);
-    })
+            this.emailGenerated.push(response.data.response);
+            console.log(this.emailGenerated);
+            console.log(this.emailGenerated.length);
+        })
+    }
   }
 }).mount('#app')
