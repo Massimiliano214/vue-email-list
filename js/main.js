d63c5218d84f12945f3067ec96777e9c580c0231
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-03-23 15:47:16 +0100

    list of email generation

--- js/main.js
+++ js/main.js
@@ -3,14 +3,34 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      emailGenerated: ''
+      emailGenerated: [
+        primaEmail = "",
+        secondaEmail = "",
+        terzaEmail = "",
+        quartaEmail = "",
+        quintaEmail = "",
+        sestaEmail = "",
+        settimaEmail = "",
+        ottavaEmail = "",
+        nonaEmail = "",
+        decimaEmail = "",
+      ]
+    }
+  },
+  methods: {
+    repeatEmail() {
+        
     }
   },
   mounted() {
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then( (response) => {
         console.log(response);
-        this.emailGenerated = response.data.response;
+        //this.emailGenerated.primaEmail = response.data.response;
+
+        this.emailGenerated.forEach((element, index) => {
+            element = response.data.response;
+        });
     })
   }
 }).mount('#app')
